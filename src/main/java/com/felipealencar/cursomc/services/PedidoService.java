1064b1028c503821c371911c06d0790227491f88
felipealencar <felipealencardev@gmail.com>
2018-02-07 23:47:28 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/felipealencar/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.felipealencar.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipealencar.cursomc.domain.Pedido;
+import com.felipealencar.cursomc.repositories.PedidoRepository;
+import com.felipealencar.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo; 
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+	
+}
