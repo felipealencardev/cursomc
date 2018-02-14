23ccf339f558232fdf47e3df9f5b5e256bab4923
felipealencar <felipealencardev@gmail.com>
2018-02-14 16:21:52 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/felipealencar/cursomc/config/DevConfig.java
+++ src/main/java/com/felipealencar/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.felipealencar.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,14 +11,22 @@ import org.springframework.context.annotation.Profile;
 import com.felipealencar.cursomc.services.DBService;
 
 @Configuration
-@Profile("dev")
-public class TestConfig {
+@Profile("test")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		this.dbService.instantiateTestDataBase();
 		return true;
 	}
