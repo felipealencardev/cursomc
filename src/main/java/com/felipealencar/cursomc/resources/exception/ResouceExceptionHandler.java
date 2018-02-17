1dbb9a206d707daf0940ecedf6a569d1801e7455
felipealencar <felipealencardev@gmail.com>
2018-02-17 15:44:58 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/felipealencar/cursomc/resources/exception/ResouceExceptionHandler.java
+++ src/main/java/com/felipealencar/cursomc/resources/exception/ResouceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.felipealencar.cursomc.services.exceptions.AuthorizationException;
 import com.felipealencar.cursomc.services.exceptions.DataIntegrityException;
 import com.felipealencar.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -39,5 +40,10 @@ public class ResouceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
-	
+	@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<StandardError> objectNotFound(AuthorizationException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	}
 }
--- src/main/java/com/felipealencar/cursomc/services/ClienteService.java
+++ src/main/java/com/felipealencar/cursomc/services/ClienteService.java
@@ -13,12 +13,15 @@ import org.springframework.stereotype.Service;
 import com.felipealencar.cursomc.domain.Cidade;
 import com.felipealencar.cursomc.domain.Cliente;
 import com.felipealencar.cursomc.domain.Endereco;
+import com.felipealencar.cursomc.domain.enums.Perfil;
 import com.felipealencar.cursomc.domain.enums.TipoCliente;
 import com.felipealencar.cursomc.dto.ClienteDTO;
 import com.felipealencar.cursomc.dto.ClienteNewDTO;
 import com.felipealencar.cursomc.repositories.CidadeRepository;
 import com.felipealencar.cursomc.repositories.ClienteRepository;
 import com.felipealencar.cursomc.repositories.EnderecoRepository;
+import com.felipealencar.cursomc.security.UserSS;
+import com.felipealencar.cursomc.services.exceptions.AuthorizationException;
 import com.felipealencar.cursomc.services.exceptions.DataIntegrityException;
 import com.felipealencar.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -38,6 +41,12 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticaded();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Cliente obj = repo.findOne(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
