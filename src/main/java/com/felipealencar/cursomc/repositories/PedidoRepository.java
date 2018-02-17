b8300a838d30463e458781d84bab93d18a2b5de5
felipealencar <felipealencardev@gmail.com>
2018-02-17 16:05:22 -0300

    Restricao de conteúdo: cliente so recupera seus pedidos

--- src/main/java/com/felipealencar/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/felipealencar/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.felipealencar.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.felipealencar.cursomc.domain.Cliente;
 import com.felipealencar.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
-
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
+	
 }
--- src/main/java/com/felipealencar/cursomc/services/PedidoService.java
+++ src/main/java/com/felipealencar/cursomc/services/PedidoService.java
@@ -3,8 +3,12 @@ package com.felipealencar.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.felipealencar.cursomc.domain.Cliente;
 import com.felipealencar.cursomc.domain.ItemPedido;
 import com.felipealencar.cursomc.domain.PagamentoComBoleto;
 import com.felipealencar.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.felipealencar.cursomc.repositories.ItemPedidoRepository;
 import com.felipealencar.cursomc.repositories.PagamentoRepository;
 import com.felipealencar.cursomc.repositories.PedidoRepository;
 import com.felipealencar.cursomc.repositories.ProdutoRepository;
+import com.felipealencar.cursomc.security.UserSS;
+import com.felipealencar.cursomc.services.exceptions.AuthorizationException;
 import com.felipealencar.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -72,4 +78,13 @@ public class PedidoService {
 		return obj;
 	}
 	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderby, String direction) {
+		UserSS user = UserService.authenticaded();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderby);
+		Cliente cliente = this.clienteRepository.findOne(user.getId());
+		return this.repo.findByCliente(cliente, pageRequest);
+	}
 }
